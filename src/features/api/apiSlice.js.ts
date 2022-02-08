f2814ea81b8a26e06283e51c8817432a7bc5da5d
liliakushnareva <l.kushnareva@akveo.com>
2022-02-08 19:14:19 +0200

    Add 'getUserQuery'

--- src/features/api/apiSlice.js.ts
+++ src/features/api/apiSlice.js.ts
@@ -2,14 +2,42 @@ import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 
 const baseUrl = 'https://api.github.com/';
 
+const headers = {
+  authorization: 'token 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',
+};
+
+export type UserResponseType = {
+  id: number;
+  login: string;
+  html_url: string;
+  name: string;
+  avatar_url: string;
+  company: string;
+  email: string;
+  following_url: string;
+  followers_url: string;
+  blog: string;
+};
+
+export type AllUsersResponseType = Array<UserResponseType>;
+
 export const apiSlice = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({baseUrl}),
   endpoints: builder => ({
-    getUsers: builder.query({
-      query: value => `/users/${value}`,
+    getUser: builder.query<UserResponseType, string>({
+      query: (value: string) => ({
+        url: `/users/${value}`,
+        headers,
+      }),
+    }),
+    getAllUsers: builder.query<AllUsersResponseType, void>({
+      query: () => ({
+        url: '/users',
+        headers,
+      }),
     }),
   }),
 });
 
-export const {useGetUsersQuery} = apiSlice;
+export const {useGetUserQuery, useGetAllUsersQuery} = apiSlice;
